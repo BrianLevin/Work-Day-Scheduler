62a13b32c7c9fbc8ca0dcb462bfead6da42bbb00
Brian Levin <bml201095@gmail.com>
2020-06-04 11:36:04 -0400

    created a variable method which will obtain the value from the textera description class

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 document.ready(function () {
+    // listen for save button clicks
     $('.saveBtn').on('click', function () {
+        // get nearby values
+        var value = $(this).siblings('.description').val();
+
 
     });
 
